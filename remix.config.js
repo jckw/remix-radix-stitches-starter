0c133acb601ba19b99cfdde944ddc00801668049
ymcheung <ymcheung.tw@me.com>
2022-02-06 11:14:57 +0800

    Move to fly.io

--- remix.config.js
+++ remix.config.js
@@ -5,6 +5,7 @@ module.exports = {
   appDirectory: 'app',
   assetsBuildDirectory: 'public/build',
   publicPath: '/build/',
-  serverBuildDirectory: 'api/_build',
+  serverBuildDirectory: 'build',
+  devServerPort: 8002,
   ignoredRouteFiles: ['.*'],
 }
